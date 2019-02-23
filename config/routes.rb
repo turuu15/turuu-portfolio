c4c150e23346cc673f471b93d25d29fe1b454e5e
turuu <turuu1529@gmail.com>
2019-02-23 23:04:55 +0800

    scaffolded blog and generated pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'pages/about'
+  get 'pages/contact'
+  get 'pages/home'
+  resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
