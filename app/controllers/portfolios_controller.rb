c1c4a7b863d1936ed5a07388dbcad4c99d97aeb4
turuu <turuu1529@gmail.com>
2019-02-24 13:16:29 +0800

    custom pages routes, portfolio show and delete functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,6 +1,13 @@
 class PortfoliosController < ApplicationController
+	
+	before_action :set_portfolio, only: [:show, :edit, :update, :destroy]
+	
 	def index
 		@portfolio_items = Portfolio.all
+	end
+
+	def show
+		
 	end
 
 
@@ -23,20 +30,33 @@ class PortfoliosController < ApplicationController
 	end
 
 	def edit
-		@portfolio_item = Portfolio.find(params[:id])
 	end
 
 	def update
-	@portfolio_item = Portfolio.find(params[:id])
-    respond_to do |format|
-      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
-        format.html { redirect_to portfolios_path, notice: 'portfolio_item was successfully updated.' }
-        format.json { render :show, status: :ok, location: @portfolio_item }
-      else
-        format.html { render :edit }
-        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+	    respond_to do |format|
+	      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+	        format.html { redirect_to portfolios_path, notice: 'portfolio_item was successfully updated.' }
+	        format.json { render :show, status: :ok, location: @portfolio_item }
+	      else
+	        format.html { render :edit }
+	        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+	      end
+	    end
+  	end
+
+	# DELETE /portfolios/1
+	# DELETE /portfolios/1.json
+	def destroy
+		@portfolio_item.destroy
+		respond_to do |format|
+			format.html { redirect_to portfolios_path, notice: 'portfolio_item was successfully destroyed.' }
+			format.json { head :no_content }
+		end
+	end
+
+	private
+		def set_portfolio
+			@portfolio_item = Portfolio.find(params[:id])
+		end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   resources :portfolios
-  get 'pages/about'
-  get 'pages/contact'
-  get 'pages/home'
+
+  get 'about', to: "pages#about"
+  get 'contact', to: "pages#contact"
+  get 'home', to: "pages#home"
+
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
