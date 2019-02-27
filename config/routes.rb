312a10618e51ad8dc89901d0fbe05c854e42d86c
turuu <turuu1529@gmail.com>
2019-02-27 23:37:28 +0800

    integerated and permitted new field to sign up and edit login button view

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
 
   resources :blogs
 
+
   get 'angular-items', to:"portfolios#angular"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
