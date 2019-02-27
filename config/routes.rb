8a0f7677d0a36a29af56a95dd10e8542bd0cf9f5
turuu <turuu1529@gmail.com>
2019-02-27 23:22:18 +0800

    implemented logout functionality and implemented dynamic view for auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  devise_for :users, path: "", path_names: {sign_in: 'login', sing_out: 'logout', sign_up: 'register'}
+  devise_for :users, path: "", path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
 
 
   resources :portfolios
